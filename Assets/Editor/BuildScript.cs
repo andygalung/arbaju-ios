10d616adcdf799c594aeca761a11a25b63761709
andygalung <andyknock70@gmail.com>
2025-12-12 22:33:54 +0700

    Update BuildScript.cs

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,19 +1,32 @@
 using UnityEditor;
+using UnityEngine;
 
-public static class BuildScript
+public class BuildScript
 {
     public static void BuildiOS()
     {
         string path = "build/ios";
 
-        BuildPlayerOptions opts = new BuildPlayerOptions
-        {
-            scenes = new[] { "Assets/Scenes/SampleScene.unity" }, // ganti sesuai scene kakak
-            locationPathName = path,
-            target = BuildTarget.iOS,
-            options = BuildOptions.None
-        };
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = GetEnabledScenes();
+        options.locationPathName = path;
+        options.target = BuildTarget.iOS;
+        options.options = BuildOptions.None;
 
-        BuildPipeline.BuildPlayer(opts);
+        BuildPipeline.BuildPlayer(options);
+    }
+
+    private static string[] GetEnabledScenes()
+    {
+        var scenes = EditorBuildSettings.scenes;
+        var enabled = new System.Collections.Generic.List<string>();
+
+        foreach (var s in scenes)
+        {
+            if (s.enabled)
+                enabled.Add(s.path);
+        }
+
+        return enabled.ToArray();
     }
 }
